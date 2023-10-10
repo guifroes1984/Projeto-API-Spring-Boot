5c2868f189a097d17eee1adc15c549eb63d4fcb7
guifroes1984 <guilhermefroes26@gmail.com>
2023-10-09 22:58:45 -0300

    Implementando atualizacao parcial com o PUT.

--- src/main/java/com/example/projeto/api/resource/PessoaResource.java
+++ src/main/java/com/example/projeto/api/resource/PessoaResource.java
@@ -67,4 +67,10 @@ public class PessoaResource {
 		return ResponseEntity.ok(pessoaSalva);
 	}
 	
+	@PutMapping("/{codigo}/ativo")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void atualizarPrpriedadeAtivo(@PathVariable Long codigo, @RequestBody Boolean ativo) {
+		pessoaService.atualizarPropriedadeAtivo(codigo, ativo);
+	}
+	
 }
