718fedb576b23d6dcf67fe777e9f6be1c0b0d9ca
guifroes1984 <guilhermefroes26@gmail.com>
2023-11-08 13:46:18 -0300

    Criando consulta para dados por categoria.

--- src/main/java/com/example/projeto/api/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/example/projeto/api/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,13 +1,19 @@
 package com.example.projeto.api.repository.lancamento;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import com.example.projeto.api.dto.LancamentoEstatisticaCategoria;
 import com.example.projeto.api.model.Lancamento;
 import com.example.projeto.api.repository.filter.LancamentoFilter;
 import com.example.projeto.api.repository.projection.ResumoLancamento;
 
 public interface LancamentoRepositoryQuery {
+	
+	public List<LancamentoEstatisticaCategoria> porCategoria(LocalDate mesReferencia);
 
 	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 	public Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable);
