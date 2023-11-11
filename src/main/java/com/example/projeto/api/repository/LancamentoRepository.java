18b3b30cc79be0918b5db8db7cfa76853b7f3799
guifroes1984 <guilhermefroes26@gmail.com>
2023-11-10 22:31:57 -0300

    Buscando lancamentos vencidos com Spring data JPA.

--- src/main/java/com/example/projeto/api/repository/LancamentoRepository.java
+++ src/main/java/com/example/projeto/api/repository/LancamentoRepository.java
@@ -1,10 +1,15 @@
 package com.example.projeto.api.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.projeto.api.model.Lancamento;
 import com.example.projeto.api.repository.lancamento.LancamentoRepositoryQuery;
 
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long>, LancamentoRepositoryQuery {
+	
+	List<Lancamento> findByDataVencimentoLessThanEqualAndDataPagamentoIsNull(LocalDate data);
 	
 }
